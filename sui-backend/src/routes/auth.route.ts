3524cdd0a016509fbe6d6396d2879724fdc98f02
raghav-rama <hackerer528@gmail.com>
2023-10-20 18:02:24 +0000

    feat: add zk routes

--- sui-backend/src/routes/auth.route.ts
+++ sui-backend/src/routes/auth.route.ts
@@ -4,6 +4,7 @@ import { CreateUserDto } from '@dtos/users.dto';
 import { Routes } from '@interfaces/routes.interface';
 import authMiddleware from '@middlewares/auth.middleware';
 import validationMiddleware from '@middlewares/validation.middleware';
+import { CreateZkUserDto } from '@/dtos/zkUsers.dto';
 
 class AuthRoute implements Routes {
   public path = '/';
@@ -17,7 +18,10 @@ class AuthRoute implements Routes {
   private initializeRoutes() {
     this.router.post(`${this.path}signup`, validationMiddleware(CreateUserDto, 'body'), this.authController.signUp);
     this.router.post(`${this.path}login`, validationMiddleware(CreateUserDto, 'body'), this.authController.logIn);
+    this.router.get(`${this.path}zk-login`, this.authController.zkLogin);
+    this.router.post(`${this.path}zk-signup`, this.authController.zkSignup);
     this.router.post(`${this.path}logout`, authMiddleware, this.authController.logOut);
+    this.router.post(`${this.path}zk-login`, this.authController.zkLogin);
   }
 }
 
