39f43ca6b8f85e6a75ed20360cacef6bc4972360
raghav-rama <hackerer528@gmail.com>
2023-10-20 18:03:46 +0000

    feat: add zk login and signup services

--- sui-backend/src/services/auth.service.ts
+++ sui-backend/src/services/auth.service.ts
@@ -7,12 +7,68 @@ import { DataStoredInToken, TokenData } from '@interfaces/auth.interface';
 import { User } from '@interfaces/users.interface';
 import userModel from '@models/users.model';
 import { isEmpty } from '@utils/util';
+import { CLIENT_ID, CLIENT_SECRET } from '@config';
+import { generateNonce, generateRandomness } from '@mysten/zklogin';
+import { SuiClient } from '@mysten/sui.js/client';
+import { Ed25519Keypair } from '@mysten/sui.js/keypairs/ed25519';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import { type JwtPayload } from 'jwt-decode';
+import { CreateZkUserDto } from '@/dtos/zkUsers.dto';
 
 class AuthService {
   public users = userModel;
+  REDIRECT_URL = 'http://localhost:3000/auth';
 
+  public async zkLogin() {
+    const FULLNODE_URL = 'https://fullnode.devnet.sui.io'; // replace with the RPC URL you want to use
+    const suiClient = new SuiClient({ url: FULLNODE_URL });
+    const { epoch, epochDurationMs, epochStartTimestampMs } = await suiClient.getLatestSuiSystemState();
+
+    const maxEpoch = Number(epoch) + 2; // this means the ephemeral key will be active for 2 epochs from now.
+    const ephemeralKeyPair = new Ed25519Keypair();
+    const randomness = generateRandomness();
+    const nonce = generateNonce(ephemeralKeyPair.getPublicKey(), maxEpoch, randomness);
+    let params = new URLSearchParams({
+      client_id: CLIENT_ID,
+      redirect_uri: this.REDIRECT_URL,
+      response_type: 'id_token',
+      scope: 'openid',
+      nonce: nonce,
+    });
+    let loginUrl = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
+    axios.get(loginUrl).then(res => {
+      console.log(res);
+    });
+  }
+
+  public async zkSignup(zkUserData: CreateZkUserDto) {
+    console.log(zkUserData);
+    const FULLNODE_URL = 'https://fullnode.devnet.sui.io'; // replace with the RPC URL you want to use
+    const suiClient = new SuiClient({ url: FULLNODE_URL });
+    const { epoch, epochDurationMs, epochStartTimestampMs } = await suiClient.getLatestSuiSystemState();
+
+    const maxEpoch = Number(epoch) + 2; // this means the ephemeral key will be active for 2 epochs from now.
+    const ephemeralKeyPair = new Ed25519Keypair();
+    const randomness = generateRandomness();
+    const nonce = generateNonce(ephemeralKeyPair.getPublicKey(), maxEpoch, randomness);
+    let params = new URLSearchParams({
+      client_id: CLIENT_ID,
+      redirect_uri: this.REDIRECT_URL,
+      response_type: 'id_token',
+      scope: 'openid',
+      nonce: nonce,
+    });
+    let loginUrl = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
+    return { loginUrl, nonce };
+    // const { data } = await axios.get(loginUrl);
+    // const { id_token } = data;
+    // console.log(id_token);
+    // const decoded = jwt_decode<JwtPayload>(id_token);
+    // console.log(decoded);
+  }
   public async signup(userData: CreateUserDto): Promise<User> {
-    if (isEmpty(userData)) throw new HttpException(400, "userData is empty");
+    if (isEmpty(userData)) throw new HttpException(400, 'userData is empty');
 
     const findUser: User = await this.users.findOne({ email: userData.email });
     if (findUser) throw new HttpException(409, `This email ${userData.email} already exists`);
@@ -24,13 +80,13 @@ class AuthService {
   }
 
   public async login(userData: CreateUserDto): Promise<{ cookie: string; findUser: User }> {
-    if (isEmpty(userData)) throw new HttpException(400, "userData is empty");
+    if (isEmpty(userData)) throw new HttpException(400, 'userData is empty');
 
     const findUser: User = await this.users.findOne({ email: userData.email });
     if (!findUser) throw new HttpException(409, `This email ${userData.email} was not found`);
 
     const isPasswordMatching: boolean = await compare(userData.password, findUser.password);
-    if (!isPasswordMatching) throw new HttpException(409, "Password is not matching");
+    if (!isPasswordMatching) throw new HttpException(409, 'Password is not matching');
 
     const tokenData = this.createToken(findUser);
     const cookie = this.createCookie(tokenData);
@@ -39,7 +95,7 @@ class AuthService {
   }
 
   public async logout(userData: User): Promise<User> {
-    if (isEmpty(userData)) throw new HttpException(400, "userData is empty");
+    if (isEmpty(userData)) throw new HttpException(400, 'userData is empty');
 
     const findUser: User = await this.users.findOne({ email: userData.email, password: userData.password });
     if (!findUser) throw new HttpException(409, `This email ${userData.email} was not found`);
