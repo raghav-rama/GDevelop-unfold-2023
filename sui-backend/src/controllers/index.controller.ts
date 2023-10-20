8bc80022ac9a75ac1e53709c62daa6b4ccebc1d5
raghav-rama <hackerer528@gmail.com>
2023-10-20 18:03:16 +0000

    test: add test button to check feature

--- sui-backend/src/controllers/index.controller.ts
+++ sui-backend/src/controllers/index.controller.ts
@@ -3,7 +3,15 @@ import { NextFunction, Request, Response } from 'express';
 class IndexController {
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.sendStatus(200);
+      res.send(`
+    <html>
+      <body>
+        <form action="/zk-signup" method="post">
+          <button type="submit">Submit POST Request to /abc</button>
+        </form>
+      </body>
+    </html>
+  `);
     } catch (error) {
       next(error);
     }
